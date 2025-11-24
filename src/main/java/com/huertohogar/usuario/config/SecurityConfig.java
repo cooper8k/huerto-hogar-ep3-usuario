49dd567f9f4d095db2b9614312733fe6e7d9256e
cooper8k <orlanprado.c@gmail.com>
2025-11-23 23:18:01 -0300

    1

--- src/main/java/com/huertohogar/usuario/config/SecurityConfig.java
+++ src/main/java/com/huertohogar/usuario/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.huertohogar.usuario.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +23,12 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                //TODOS LOS ENDPOINTS PUBLICOS - NO REQUIEREN TOKEN
+                .requestMatchers(HttpMethod.POST, "/api/v1/usuario/login").permitAll()
+
                 .anyRequest().permitAll()
-            );
+            )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
