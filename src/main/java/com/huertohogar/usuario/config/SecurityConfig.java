6266263eb670928258551f8899862104ffa47289
cooper8k <orlanprado.c@gmail.com>
2025-11-23 22:58:19 -0300

    jwt sin autenticacion para los endpiont solo se genera no se utiliza en el back

--- src/main/java/com/huertohogar/usuario/config/SecurityConfig.java
+++ src/main/java/com/huertohogar/usuario/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.huertohogar.usuario.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,16 +21,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                //TODOS LOS ENDPOINTS PUBLICOS - NO REQUIEREN TOKEN
-                .requestMatchers(HttpMethod.POST, "/api/v1/usuario/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/v1/usuario/guardar").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/usuario/**").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/usuario/**").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/v1/usuario/**").permitAll()
-
                 .anyRequest().permitAll()
-            )
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            );
         return http.build();
     }
 
